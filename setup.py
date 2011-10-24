49161441568e8b71ec084eeffce743385bf9b221
Michael Joseph Walsh <github.com{nospam}nemonik.com>
2011-10-24 15:17:22 -0700

    fixing package build issues

--- setup.py
+++ setup.py
@@ -35,11 +35,10 @@ from setuptools import setup, find_packages
 
 DESCRIPTION = 'A Domain-specific language and Rules Engine for Python'
 
-readme_file = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
-print readme_file
-
-LONG_DESCRIPTION = readme_file.read()
-readme_file.close()
+if os.path.exists("README.rst"):
+  long_description = open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst')).read()
+else:
+  long_description = "See http://pypi.python.org/pypi/Intellect"
 
 def get_version(version_tuple):
     version = '%s.%s' % (version_tuple[0], version_tuple[1])
@@ -47,7 +46,21 @@ def get_version(version_tuple):
         version = '%s.%s' % (version, version_tuple[2])
     return version
 
-init = os.path.join(os.path.dirname(__file__), 'src', 'intellect', '__init__.py')
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+init = os.path.join(os.path.dirname(__file__), 'intellect', '__init__.py')
 version_line = filter(lambda l: l.startswith('VERSION'), open(init))[0]
 VERSION = get_version(eval(version_line.split('=')[-1]))
 print VERSION
@@ -62,18 +75,32 @@ CLASSIFIERS = [
     "Topic :: Software Development :: Libraries :: Python Modules"
 ]
 
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+
+intellect_dir = 'intellect'
+
+for dirpath, dirnames, filenames in os.walk(intellect_dir):
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
 setup(name="Intellect",
       version=VERSION,
       description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
+      long_description=long_description,
       author='Michael Joseph Walsh',
       author_email='github.com{nospam}nemonik.com',
       url='http://github.com/nemonik/Intellect',
       classifiers=CLASSIFIERS,
       keywords='intellect rules engine dsl policy',
       license='BSD, 4-clause license',
-      packages=find_packages('src'),
-      package_dir = {'':'src'},
-      package_data = {'': ['*.g', '*.tokens', '*.rst']},
+      packages = packages,
+      package_data = {'': ['*.g', '*.tokens']},
       include_package_data=True,
       install_requires=['antlr_python_runtime>=3.1.3'])
